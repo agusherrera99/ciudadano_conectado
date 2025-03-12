8970c68366f7086cbb7e7d54e8ad808a3c36687a
agusherrera99 <agustinherrera.dev@gmail.com>
2025-03-12 12:16:07 -0300

    Feature: Add verbose names and ordering to Notification model, update NotificationsConfig verbose name

--- notifications/models.py
+++ notifications/models.py
@@ -3,16 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Notification(models.Model):
-    user = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE)
-    issue = models.ForeignKey('issues.Issue', on_delete=models.CASCADE, null=True, blank=True)
-    survey = models.ForeignKey('surveys.Survey', on_delete=models.CASCADE, null=True, blank=True)
-    volunteering = models.ForeignKey('volunteering.Volunteering', on_delete=models.CASCADE, null=True, blank=True)
-    message = models.TextField()
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = 'Notificación'
+        verbose_name_plural = 'Notificaciones'
+        ordering = ('-created_at',)
+
+    user = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE, verbose_name='Usuario')
+    issue = models.ForeignKey('issues.Issue', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Solicitud')
+    survey = models.ForeignKey('surveys.Survey', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Encuesta')
+    volunteering = models.ForeignKey('volunteering.Volunteering', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Voluntariado')
+    message = models.TextField(verbose_name='Mensaje')
+    is_read = models.BooleanField(default=False, verbose_name='Leído')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
 
     def __str__(self):
         return f"Notification: #{self.id} - User: {self.user.username}"
-    
-    class Meta:
-        ordering = ('-created_at',)
