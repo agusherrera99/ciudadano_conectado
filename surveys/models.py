29e204edc76b41f589474657a9589803c1d41ab6
agusherrera99 <agustinherrera.dev@gmail.com>
2025-03-12 12:20:05 -0300

    Feature: Add verbose names for Survey, Question, Option, and Answer models, and update SurveysConfig verbose name for localization

--- surveys/models.py
+++ surveys/models.py
@@ -2,19 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Survey(models.Model):
+    class Meta:
+        verbose_name = 'Encuesta'
+        verbose_name_plural = 'Encuestas'
+
     STATUS_CHOICES = (
         ('activa', 'Activa'),
         ('finalizada', 'Finalizada'),
     )
 
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    status = models.CharField(max_length=60, choices=STATUS_CHOICES, default='activa')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    pollster = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE, related_name='pollster', blank=True, null=True)
+    name = models.CharField(max_length=255, verbose_name='Nombre')
+    description = models.TextField(verbose_name='Descripción')
+    start_date = models.DateTimeField(verbose_name='Fecha de inicio')
+    end_date = models.DateTimeField(verbose_name='Fecha de finalización')
+    status = models.CharField(max_length=60, choices=STATUS_CHOICES, default='activa', verbose_name='Estado')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
+    pollster = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE, related_name='pollster', blank=True, null=True, verbose_name='Encuestador')
 
     def __str__(self):
         return self.name
@@ -25,16 +29,20 @@ class Survey(models.Model):
         super().save(*args, **kwargs)
     
 class Question(models.Model):
+    class Meta:
+        verbose_name = 'Pregunta'
+        verbose_name_plural = 'Preguntas'
+
     QUESTION_TYPES = (
         ('predefinida', 'Predefinida'),
         ('libre', 'Libre')
     )
 
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
-    question_text = models.TextField()
-    question_type = models.CharField(max_length=50, choices=QUESTION_TYPES)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, verbose_name='Encuesta', related_name='questions')
+    question_text = models.TextField(verbose_name='Pregunta')
+    question_type = models.CharField(max_length=50, choices=QUESTION_TYPES, verbose_name='Tipo de pregunta')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
 
     def __str__(self):
         return self.question_text
@@ -44,9 +52,13 @@ class Question(models.Model):
         super().save(*args, **kwargs)
 
 class Option(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='options')
-    option_text = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = 'Opción'
+        verbose_name_plural = 'Opciones'
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='options', verbose_name='Pregunta')
+    option_text = models.CharField(max_length=255, blank=True, null=True, verbose_name='Opción')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     
     def __str__(self):
         return self.option_text
@@ -56,10 +68,14 @@ class Option(models.Model):
         super().save(*args, **kwargs)
 
 class Answer(models.Model):
-    user = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer_text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = 'Respuesta'
+        verbose_name_plural = 'Respuestas'
+
+    user = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE, verbose_name='Usuario')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Pregunta')
+    answer_text = models.TextField(verbose_name='Respuesta')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     
     def __str__(self):
         return self.answer_text
