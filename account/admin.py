295573237a32f1162340950cc42c9c97f939b629
agusherrera99 <agustinherrera.dev@gmail.com>
2025-03-13 14:28:55 -0300

    Feature: Register InternalUser, Department, and Position models in admin with custom configurations

--- account/admin.py
+++ account/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-
-from .models import CustomUser
+from .models import CustomUser, InternalUser, Department, Position
 
 
 @admin.register(CustomUser)
@@ -31,7 +30,7 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {"fields": ("username", "email", "password")}),
         ("Personal info", {"fields": ("first_name", "last_name", "phone_number", "address", "dni")}),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_internal", "is_superuser", "groups", "user_permissions")}),
+        ("Permissions", {"fields": ("is_active", "groups")}),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
 
@@ -40,4 +39,90 @@ class CustomUserAdmin(UserAdmin):
             "classes": ("wide",),
             "fields": ("username", "email", "password1", "password2"),
         }),
+    )
+
+@admin.register(InternalUser)
+class InternalUserAdmin(UserAdmin):
+    list_display = [
+        "username",
+        "email",
+        "department",
+        "position",
+        "created_at",
+        "updated_at",
+        "get_groups",
+        "is_staff",
+    ]
+    search_fields = ["username", "email", "department__name", "position__name"]
+    list_filter = ["department", "position", "created_at", "updated_at", "is_staff"]
+    ordering = ["created_at"]
+    list_per_page = 25
+    list_display_links = ["username"]
+
+    show_facets = admin.ShowFacets.ALWAYS
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "phone_number", "address", "dni")}),
+        ("Departamento", {"fields": ("department", "position")}),
+        ("Permissions", {"fields": ("is_active", "groups")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2"),
+        }),
+    )
+
+    def get_groups(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+    get_groups.short_description = "Grupos"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(id=request.user.id)
+
+    def save_model(self, request, obj, form, change):
+        obj.is_internal = True  # Asegurar que siempre se guarde como interno
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "created_at",
+        "updated_at"
+    ]
+    search_fields = ["name"]
+    ordering = ["created_at"]
+    list_per_page = 25
+    list_display_links = ["name"]
+
+    show_facets = admin.ShowFacets.ALWAYS
+
+    fieldsets = (
+        (None, {"fields": ("name",)}),
+    )
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "created_at",
+        "updated_at"
+    ]
+    search_fields = ["name"]
+    ordering = ["created_at"]
+    list_per_page = 25
+    list_display_links = ["name"]
+
+    show_facets = admin.ShowFacets.ALWAYS
+
+    fieldsets = (
+        (None, {"fields": ("name",)}),
     )
