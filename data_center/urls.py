f88c76c4fed58c7f42fee240f4ae9bf23018fbb7
agusherrera99 <agustinherrera.dev@gmail.com>
2025-03-27 10:07:30 -0300

    Add: implement areas view with navigation and styling, including back button and module cards

--- data_center/urls.py
+++ data_center/urls.py
@@ -6,4 +6,5 @@ app_name = 'data_center'
 
 urlpatterns = [
     path('', views.data_center, name='data_center'),
+    path('areas/', views.areas, name='areas'),
 ]
--- data_center/views.py
+++ data_center/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def data_center(request):
-    return render(request, 'data_center.html')
+    context = {
+        'url_link': reverse('pages:transparency')
+    }
+    return render(request, 'data_center.html', context=context)
+
+def areas(request):
+    context = {
+        'url_link': reverse('data_center:data_center')
+    }
+    return render(request, 'areas.html', context=context)
